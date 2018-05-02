7ab53a5eace097ea0490f6f3314c83388bba299d
12976 <1297684673@qq.com>
2018-05-02 11:18:19 +0800

    js

--- app/src/main/java/com/kas/clientservice/haiyansmartenforce/Module/MainActivity.java
+++ app/src/main/java/com/kas/clientservice/haiyansmartenforce/Module/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import com.jorge.circlelibrary.ImageCycleView;
 import com.kas.clientservice.haiyansmartenforce.Base.BaseActivity;
 import com.kas.clientservice.haiyansmartenforce.Module.CaseCommit.CaseCommitActivity;
+import com.kas.clientservice.haiyansmartenforce.Module.History.HistoryActivity;
 import com.kas.clientservice.haiyansmartenforce.Module.IllegalParking.IllegalParkingCommitActivity;
 import com.kas.clientservice.haiyansmartenforce.Module.IllegalParking.ParkingRecordSearchActivity;
 import com.kas.clientservice.haiyansmartenforce.R;
@@ -33,6 +34,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     LinearLayout ll_wentishangbao;
     @BindView(R.id.banner_main)
     UPMarqueeView upview1;
+    @BindView(R.id.ll_lishijilu)
+    LinearLayout ll_lishijilu;
 
 
     ArrayList<String> list_detail;
@@ -57,7 +60,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         ll_caseSearch.setOnClickListener(this);
         ll_quickCommit.setOnClickListener(this);
         ll_wentishangbao.setOnClickListener(this);
-
+        ll_lishijilu.setOnClickListener(this);
         initBanner();
         initVerticalBanner();
     }
@@ -110,6 +113,10 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 break;
             case R.id.ll_wentishangbao:
                 startActivity(new Intent(mContext, CaseCommitActivity.class));
+                break;
+            case R.id.ll_lishijilu:
+                startActivity(new Intent(mContext, HistoryActivity.class));
+                break;
         }
     }
 
--- app/src/main/java/com/kas/clientservice/haiyansmartenforce/Module/Register/RegisterActivity.java
+++ app/src/main/java/com/kas/clientservice/haiyansmartenforce/Module/Register/RegisterActivity.java
@@ -1,10 +1,23 @@
 package com.kas.clientservice.haiyansmartenforce.Module.Register;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import com.kas.clientservice.haiyansmartenforce.Base.BaseActivity;
+import com.kas.clientservice.haiyansmartenforce.Module.Login.LoginActivity;
 import com.kas.clientservice.haiyansmartenforce.R;
 
-public class RegisterActivity extends BaseActivity {
+import butterknife.BindView;
 
+public class RegisterActivity extends BaseActivity implements View.OnClickListener{
+    @BindView(R.id.tv_login_login)
+    TextView tv_login_login;
+    @BindView(R.id.tv_header_title)
+    TextView tv_title;
+    @BindView(R.id.iv_heaer_back)
+    ImageView iv_back;
     @Override
     protected int getLayoutId() {
         return R.layout.activity_register;
@@ -18,6 +31,18 @@ public class RegisterActivity extends BaseActivity {
     @Override
     protected void initResAndListener() {
         super.initResAndListener();
+        tv_title.setText("注册");
+        tv_login_login.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.tv_login_login:
+                startActivity(new Intent(this, LoginActivity.class));
+                finish();
+                break;
+
+        }
     }
 }
