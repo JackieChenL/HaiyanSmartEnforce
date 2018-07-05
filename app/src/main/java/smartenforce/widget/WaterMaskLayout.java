b558d41938aa432b42d3ecf6cdc9fd24badd6957
ldyzxn <179718898@qq.com>
2018-07-05 11:19:12 +0800

    修改添加水印样式

--- app/src/main/java/smartenforce/widget/WaterMaskLayout.java
+++ app/src/main/java/smartenforce/widget/WaterMaskLayout.java
@@ -34,16 +34,17 @@ public class WaterMaskLayout extends LinearLayout {
         String userName = UserSingleton.getInstance().getUserAccount(context);
         int W = UIUtil.getScreenWidth(context);
         int H = UIUtil.getScreenHeight(context);
-        int WaterTextH = UIUtil.dip2px(context, 60);
+        int WaterTextH = UIUtil.dip2px(context, 80);
         int WaterTextW = UIUtil.dip2px(context, W / 3);
         int count = H / WaterTextH + 1;
         for (int i = 0; i < count; i++) {
             int currentWaterTextW = (i % 3 + 1) * WaterTextW;
             WaterMaskTextView textView = new WaterMaskTextView(context);
             textView.setLayoutParams(new LinearLayoutCompat.LayoutParams(currentWaterTextW, WaterTextH));
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
-            textView.setTextColor(Color.parseColor("#00BFFF"));
-            textView.setText(userName);
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,16);
+            textView.setTextColor(Color.parseColor("#AF00BFFF"));
+            textView.setDegree(350);
+            textView.setText("海盐县综合行政执法局\n 严禁非法获取个人信息["+userName+"]");
             this.addView(textView, i);
         }
 
