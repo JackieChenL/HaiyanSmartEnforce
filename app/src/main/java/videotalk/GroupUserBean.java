b4c15e3b15afe31003a7b7692a551c40cfb948bf
ldyzxn <179718898@qq.com>
2018-07-27 11:26:51 +0800

    身份证识别api替换，在线视屏模式修改

---
+++ app/src/main/java/videotalk/GroupUserBean.java
@@ -0,0 +1,11 @@
+package videotalk;
+
+
+public class GroupUserBean {
+    //记录是否选中视屏通话
+    public boolean isSelect=false;
+    public String id;
+    public String name;
+    public String token;
+
+}
