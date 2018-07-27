39e747f385c4d5761b9948c4021b2aa3c8169724
ldyzxn <179718898@qq.com>
2018-07-27 11:26:51 +0800

    身份证识别api替换，在线视屏模式修改

---
+++ app/src/main/java/videotalk/UserLiveStatusBean.java
@@ -0,0 +1,13 @@
+package videotalk;
+
+
+
+public class UserLiveStatusBean {
+    public int code;
+    public String status;
+
+    public String getStatus(){
+
+        return status.equals("1")?"[在线]":"[离线]";
+    }
+}
