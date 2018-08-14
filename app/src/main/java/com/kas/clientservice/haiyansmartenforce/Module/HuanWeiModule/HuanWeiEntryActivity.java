24d6773b5448958d9969ce8ab9eaed3ce52df999
chenlong <371687441@qq.com>
2018-08-14 08:41:24 +0800

    UI修改

--- app/src/main/java/com/kas/clientservice/haiyansmartenforce/Module/HuanWeiModule/HuanWeiEntryActivity.java
+++ app/src/main/java/com/kas/clientservice/haiyansmartenforce/Module/HuanWeiModule/HuanWeiEntryActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.kas.clientservice.haiyansmartenforce.Base.BaseActivity;
@@ -21,9 +22,16 @@ public class HuanWeiEntryActivity extends BaseActivity implements View.OnClickLi
     TextView tv_title;
     @BindView(R.id.iv_heaer_back)
     ImageView iv_back;
+    @BindView(R.id.ll_huanwei_entry_handle)
+    LinearLayout ll_handle;
+    @BindView(R.id.ll_huanwei_entry_history)
+    LinearLayout ll_history;
+    @BindView(R.id.iv_huanwei_entry)
+            ImageView iv_handle;
 
     String type = "";
     int typeId = 0;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_huan_wei_entry;
@@ -39,24 +47,27 @@ public class HuanWeiEntryActivity extends BaseActivity implements View.OnClickLi
         super.initResAndListener();
 
 
-        if (UserSingleton.USERINFO.getReviewName()!=null&&!UserSingleton.USERINFO.getReviewName().getType().equals("0")) {
+        if (UserSingleton.USERINFO.getReviewName() != null && !UserSingleton.USERINFO.getReviewName().getType().equals("0")) {
             type = UserSingleton.USERINFO.getReviewName().getType();
         }
-        if (UserSingleton.USERINFO.getChangeName()!=null&&!UserSingleton.USERINFO.getChangeName().getType().equals("0")) {
+        if (UserSingleton.USERINFO.getChangeName() != null && !UserSingleton.USERINFO.getChangeName().getType().equals("0")) {
             type = UserSingleton.USERINFO.getChangeName().getType();
         }
-        if (UserSingleton.USERINFO.getCheckName()!=null&&!UserSingleton.USERINFO.getCheckName().getType().equals("0")) {
+        if (UserSingleton.USERINFO.getCheckName() != null && !UserSingleton.USERINFO.getCheckName().getType().equals("0")) {
             type = UserSingleton.USERINFO.getCheckName().getType();
         }
-        Log.i(TAG, "initResAndListener: "+type);
+        Log.i(TAG, "initResAndListener: " + type);
         if (type.equals("5")) {
             tv_handle.setText("审核");
+            iv_handle.setImageResource(R.drawable.shenghe);
             typeId = 5;
             type = UserSingleton.USERINFO.getReviewName().getType();
-        }else if (type.equals("6")){
+        } else if (type.equals("6")) {
             tv_handle.setText("检查录入");
+            iv_handle.setImageResource(R.drawable.luru);
             typeId = 6;
-        }else if (type.equals("7")){
+        } else if (type.equals("7")) {
+            iv_handle.setImageResource(R.drawable.zhenggai);
             tv_handle.setText("整改");
             typeId = 7;
         }
@@ -64,6 +75,8 @@ public class HuanWeiEntryActivity extends BaseActivity implements View.OnClickLi
         iv_back.setOnClickListener(this);
         tv_handle.setOnClickListener(this);
         tv_search.setOnClickListener(this);
+        ll_handle.setOnClickListener(this);
+        ll_history.setOnClickListener(this);
     }
 
     @Override
@@ -72,27 +85,29 @@ public class HuanWeiEntryActivity extends BaseActivity implements View.OnClickLi
             case R.id.iv_heaer_back:
                 finish();
                 break;
+            case R.id.ll_huanwei_entry_handle:
             case R.id.tv_huanwei_handle:
-                Log.i(TAG, "onClick: "+typeId);
+                Log.i(TAG, "onClick: " + typeId);
                 switch (typeId) {
                     case 5:
-                        Intent intent = new Intent(mContext,HuanweiCheckListActivity.class);
-                        intent.putExtra("Type",typeId);
+                        Intent intent = new Intent(mContext, HuanweiCheckListActivity.class);
+                        intent.putExtra("Type", typeId);
                         startActivity(intent);
                         break;
                     case 6:
-                        startActivity(new Intent(mContext,HuanweiCommitActivity.class));
+                        startActivity(new Intent(mContext, HuanweiCommitActivity.class));
                         break;
                     case 7:
-                        Intent intent2 = new Intent(mContext,HuanweiCheckListActivity.class);
-                        intent2.putExtra("Type",typeId);
+                        Intent intent2 = new Intent(mContext, HuanweiCheckListActivity.class);
+                        intent2.putExtra("Type", typeId);
                         startActivity(intent2);
                         break;
                 }
                 break;
+            case R.id.ll_huanwei_entry_history:
             case R.id.tv_huanwei_search:
-                Intent intent = new Intent(mContext,HuanweiHistoryActivity.class);
-                intent.putExtra("type",typeId);
+                Intent intent = new Intent(mContext, HuanweiHistoryActivity.class);
+                intent.putExtra("type", typeId);
                 startActivity(intent);
                 break;
 
